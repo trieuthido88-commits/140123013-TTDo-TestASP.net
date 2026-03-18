79f8f74a1568b7e483535b0ea2f5fbc12a05ca6e
trieuthido88-commits <trieuthido88@gmail.com>
2026-03-18 20:12:47 +0700

    CapnhatAdminvaPhancongGV

---
+++ Nop bai/WebApplication1/Admin/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.Admin
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["user"] != null)
+            {
+                if (Session["role"].ToString() != "1")
+                {
+                    Response.Redirect("../Default.aspx");
+                }
+            }
+        }
+    }
+}
---
+++ Nop bai/WebApplication1/Admin/QLNguoidung.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.Admin
+{
+    public partial class QLNguoidung : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Session["user"] != null)
+                {
+                    if (Session["role"].ToString() != "1")
+                    {
+                        Response.Redirect("../Default.aspx");
+                    }
+                }
+                else
+                {
+                    Response.Redirect("../default.aspx");
+                }
+            }
+        }
+    }
+}
